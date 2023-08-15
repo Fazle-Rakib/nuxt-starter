595a71097c29fd7a9fa14be43e59fb7aa764cda0
Fazle Rabbi Rakib <fazle.rabbi@pathao.com>
2023-08-15 14:57:12 +0600

    useFetch pending return value

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,10 +1,14 @@
 <template>
     <div>
         <h1>Products <span class="font-bold italic">{{id}}</span></h1>
-        <p class="bg-blue-400 text-gray-100 p-4 shadow-sm flex flex-col">
+        <p v-if="!pending" class="bg-blue-400 text-gray-100 p-4 shadow-sm flex flex-col rounded">
             <span>{{ product.title}}</span>
             <span>{{ product.price}}</span>
-            <span>{{ product.category}}</span>  
+            <span>{{ product.category}}</span> 
+            <img :src="product.image" alt="" class="rounded max-w-sm"> 
+        </p>
+        <p v-else>
+            Loading!!!!!!!!
         </p>
     </div>
 </template>
@@ -16,7 +20,7 @@
 
     const { id } = useRoute().params
     const uri = 'https://fakestoreapi.com/products/' + id;
-    const { data: product } = await useFetch(uri);
+    const { data: product, pending } = await useFetch(uri);
 </script>
 
 <style scoped>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,7 +3,9 @@
         <header class="font-bold my-4">Products</header>
         <div class="grid grid-cols-4 gap-5">
             <div 
-            v-for="product in products" 
+            v-for="product in products"
+            :key="product.id"
+            class="bg-blue-100 rounded p-2"
             >
                 <NuxtLink :to="`/products/${product.id}`"> {{product.title}} </NuxtLink>
             </div>
